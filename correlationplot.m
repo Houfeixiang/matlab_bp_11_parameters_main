aa0775a02690e9573a55e751ce6ee010dea3af68
Houfeixiang <45409278+Houfeixiang@users.noreply.github.com>
2019-02-18 10:09:11 +0800

    Create correlationplot.m

---
+++ correlationplot.m
@@ -0,0 +1,11 @@
+function [c]=correlationplot(a,b)
+plot(a,b,'ro')
+R1=corrcoef(a,b); %求得ac/dc和bp峰值的相关性
+c=R1(1,2);
+hold on
+[p1,S1]=polyfit(a,b,1);%求得拟合参数
+y1=polyval(p1,a);%求得拟合的直线   
+plot(a,y1);
+set(gca,'FontSize',20);%横纵坐标轴字号，15
+set(gca,'Fontname','Times New Roman');%横纵坐标轴字体，新罗马字体
+end
